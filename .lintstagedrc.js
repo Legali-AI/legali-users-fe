288bb44b06212eb42a8cd357445d46548f842480
farhanazizf <farhanazizf7@gmail.com>
2025-09-25 10:29:59 +0700

    chore(precommit): comment build it's better on ci/cd

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
   "!(src/sdk|**/sdk)/**/*.{js,jsx,ts,tsx}": [
     "biome check --write",
     () => "bun run type-check",
-    () => "bun run build",
+    // () => "bun run build", // build better on CI/CD
   ],
   "**/*.{json,md,css,scss,yaml,yml}": ["prettier --write"],
 };
